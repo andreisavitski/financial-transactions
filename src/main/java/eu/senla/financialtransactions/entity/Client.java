1ac389677399b942a71296b84ec1460dcdee872f
andreisavitski <andsavi93@gmail.com>
2024-12-06 10:02:07 +0300

    init

--- src/main/java/eu/senla/financialtransactions/entity/Client.java
+++ src/main/java/eu/senla/financialtransactions/entity/Client.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Table(name = "client")
 @Entity
 public class Client {
