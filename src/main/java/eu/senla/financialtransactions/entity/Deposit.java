8fcb986d1f972c3f10d9f4aeda8bd823b7700194
andreisavitski <andsavi93@gmail.com>
2025-03-10 17:57:26 +0300

    The deposit entity has been expanded with the fields amount, term and purpose. The ability to enter this data when creating a deposit has been added.

--- src/main/java/eu/senla/financialtransactions/entity/Deposit.java
+++ src/main/java/eu/senla/financialtransactions/entity/Deposit.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.UUID;
 
 @Getter
@@ -18,4 +22,24 @@ public class Deposit {
     @Id
     @Column(name = "id")
     private UUID id;
+
+    @Column(name = "deposit_opening_date")
+    private LocalDate depositOpeningDate;
+
+    @Column(name = "deposit_closing_date")
+    private LocalDate depositClosingDate;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+    @Transient
+    private Long term;
+
+    @Column(name = "purpose")
+    private String purpose;
+
+    public Long getTerm() {
+        final Period period = Period.between(depositOpeningDate, depositClosingDate);
+        return (long) period.getMonths();
+    }
 }
