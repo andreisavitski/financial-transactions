34eeec79169661a9cc6e8ef535a099a3db0d1403
Andrei Savitski <130839748+andreisavitski@users.noreply.github.com>
2025-03-14 14:32:30 +0300

    2025.6: delete @NotNull(#23)
    * delete @NotNull

--- src/main/java/eu/senla/financialtransactions/controller/DepositRestController.java
+++ src/main/java/eu/senla/financialtransactions/controller/DepositRestController.java
@@ -5,7 +5,6 @@ import eu.senla.financialtransactions.dto.DepositUpdaterMessageDto;
 import eu.senla.financialtransactions.dto.MessageResponseDto;
 import eu.senla.financialtransactions.service.DepositService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
