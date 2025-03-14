c0dd59a3ed05820fe6ae0d1e962e6e8befffb045
Andrei Savitski <130839748+andreisavitski@users.noreply.github.com>
2025-03-14 11:38:03 +0300

    2025.5 (#15)
    * delete @NotNull

--- src/main/java/eu/senla/financialtransactions/controller/ActionRestController.java
+++ src/main/java/eu/senla/financialtransactions/controller/ActionRestController.java
@@ -4,7 +4,6 @@ import eu.senla.financialtransactions.entity.Action;
 import eu.senla.financialtransactions.service.ActionService;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -32,27 +31,23 @@ public class ActionRestController {
 
     private final ActionService actionService;
 
-    @NotNull
     @GetMapping("/client")
-    public List<Action> getActionByClientId(
-            @NotNull @RequestParam(value = CLIENT_ID) Long clientId) {
+    public List<Action> getActionByClientId(@RequestParam(value = CLIENT_ID) Long clientId) {
         return actionService.findActionByClientId(clientId);
     }
 
-    @NotNull
     @GetMapping("/page")
     public Page<Action> getActionsByClientId(
-            @NotNull @RequestParam(value = CLIENT_ID) Long clientId,
-            @NotNull @RequestParam(value = OFFSET,
+            @RequestParam(value = CLIENT_ID) Long clientId,
+            @RequestParam(value = OFFSET,
                     defaultValue = DEFAULT_OFFSET) @Min(MIN_OFFSET) Integer offset,
-            @NotNull @RequestParam(value = LIMIT,
+            @RequestParam(value = LIMIT,
                     defaultValue = DEFAULT_LIMIT) @Min(MIN_LIMIT) @Max(MAX_LIMIT) Integer limit) {
         return actionService.findActionByClientId(clientId, PageRequest.of(offset, limit));
     }
 
-    @NotNull
     @GetMapping("/id")
-    public Action getActionId(@NotNull @RequestParam(value = ID) String id) {
+    public Action getActionId(@RequestParam(value = ID) String id) {
         return actionService.findActionById(id);
     }
 }
